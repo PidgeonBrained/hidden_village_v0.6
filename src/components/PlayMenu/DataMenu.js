2ce708681cfe053d7a3801ea6f6262b4987750e4
PidgeonBrained <Ventex240@gmail.com>
2024-10-25 09:04:14 -0500

    TextBoxes using pixi-text-input
    They're broken!!

--- src/components/PlayMenu/DataMenu.js
+++ src/components/PlayMenu/DataMenu.js
@@ -6,7 +6,9 @@ import { useCallback } from "react";
 import { TextStyle } from "@pixi/text";
 import InputBox from "../InputBox";
 import { Input } from 'postcss';
-
+import { useEffect, useRef } from 'react';
+import PixiTextInput from '../PixiTextInput';
+import { userEmail } from "firebase/database"; //Doesnt work?
 
 const DataMenu = (props) => {
   const {
@@ -17,15 +19,18 @@ const DataMenu = (props) => {
     trigger
   } = props;
 
+
   const innerRectWidth = menuWidth * 0.94;
   const innerRectHeight = menuHeight * 0.8; 
   const innerRectMargins = (menuWidth - innerRectWidth) / 2;
   const fieldTextMarginsFromInnerRect = menuHeight * 0.07;
-  const fieldTextMarginsFromEachOther = menuHeight * 0.13;
+  const fieldTextMarginsFromEachOther = menuHeight * 0.11;
   const fieldHeight = menuWidth * 0.028;
-  const inputBoxHeight = fieldHeight * 3;
+  const inputBoxHeight = fieldHeight * 2;
   const inputBoxTextSize = menuWidth * 0.02;
-  const distanceFromFieldTextToField = menuWidth * 0.5;
+  const distanceFromFieldTextToField = menuWidth * 0.4;
+  const checkButtonWidth = menuWidth * 0.07;
+  const checkButtonFont = menuWidth * 0.05;
   const fieldText = new TextStyle({
     align: "center",                    
     fontFamily: "Arial",                 
@@ -102,26 +107,49 @@ const DataMenu = (props) => {
         y={y + menuHeight - innerRectMargins - innerRectHeight + fieldTextMarginsFromInnerRect + fieldTextMarginsFromEachOther * 3}
         anchor={0}
       />
-        <InputBox
-          height={inputBoxHeight}
-          width={(innerRectWidth - distanceFromFieldTextToField - fieldTextMarginsFromInnerRect * 2) / 0.4}
-          x={x + innerRectMargins + fieldTextMarginsFromInnerRect + distanceFromFieldTextToField}
-          y={y + menuHeight - innerRectMargins - innerRectHeight + fieldTextMarginsFromInnerRect}
-          color={white}
-          fontSize={inputBoxTextSize}  //  Dynamically modify font size based on screen width
-          fontColor={black}
-          text={"null"}
-          fontWeight={600}
-        />
-        {/* <input
-        value={email}
-        onChange={handleEmailChange}
-        id="email"
-        className="login-input-input"
-        type="email"
-        autoComplete="on"
-        /> */}
-
+      <PixiTextInput
+        height={inputBoxHeight}
+        width={(innerRectWidth - distanceFromFieldTextToField - fieldTextMarginsFromInnerRect * 2)}
+        x={x + innerRectMargins + fieldTextMarginsFromInnerRect + distanceFromFieldTextToField}
+        y={y + menuHeight - innerRectMargins - innerRectHeight + fieldTextMarginsFromInnerRect - (fieldHeight / 2)}
+        value={userEmail}
+        style={"disabled"}
+        onChange={(newValue) => console.log(newValue)}
+      />
+      {/* <PixiTextInput
+        height={inputBoxHeight}
+        width={(innerRectWidth - distanceFromFieldTextToField - fieldTextMarginsFromInnerRect * 2)}
+        x={x + innerRectMargins + fieldTextMarginsFromInnerRect + distanceFromFieldTextToField}
+        y={y + menuHeight - innerRectMargins - innerRectHeight + fieldTextMarginsFromInnerRect + fieldTextMarginsFromEachOther - (fieldHeight / 2)}
+        value={"null"}
+        onChange={(newValue) => console.log(newValue)}
+      />
+      <PixiTextInput
+        height={inputBoxHeight}
+        width={(innerRectWidth - distanceFromFieldTextToField - fieldTextMarginsFromInnerRect * 2)}
+        x={x + innerRectMargins + fieldTextMarginsFromInnerRect + distanceFromFieldTextToField}
+        y={y + menuHeight - innerRectMargins - innerRectHeight + fieldTextMarginsFromInnerRect + fieldTextMarginsFromEachOther * 2 - (fieldHeight / 2)}
+        value={"null"}
+        onChange={(newValue) => console.log(newValue)}
+      />
+      <PixiTextInput
+        height={inputBoxHeight}
+        width={(innerRectWidth - distanceFromFieldTextToField - fieldTextMarginsFromInnerRect * 2)}
+        x={x + innerRectMargins + fieldTextMarginsFromInnerRect + distanceFromFieldTextToField}
+        y={y + menuHeight - innerRectMargins - innerRectHeight + fieldTextMarginsFromInnerRect + fieldTextMarginsFromEachOther * 3 - (fieldHeight / 2)}
+        value={"null"}
+        onChange={(newValue) => console.log(newValue)}
+      /> */}
+      {/* <InputBox
+        height={checkButtonWidth}
+        width={checkButtonWidth}
+        x={x + innerRectMargins + fieldTextMarginsFromInnerRect}
+        y={y + menuHeight - innerRectMargins - innerRectHeight + fieldTextMarginsFromInnerRect + fieldTextMarginsFromEachOther * 4}
+        color={white}
+        fontSize={checkButtonFont}
+        fontColor={black}
+        text={"X"}
+      /> */}
     </Container>
     )
 } else {
